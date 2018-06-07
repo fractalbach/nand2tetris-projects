26300947dc835456f318996468d359d13f7eb4a5
fractalbach <4bach@protonmail.com>
2018-06-07 01:49:21 -0700

    SimpleFunction tests passed!

--- 08/FunctionCalls/SimpleFunction/SimpleFunction.asm
+++ 08/FunctionCalls/SimpleFunction/SimpleFunction.asm
@@ -1,52 +1,3 @@
-// bootstrap code
-// ----------------------------------
-// set stack pointer to 256
-@256
-D=A
-@SP
-M=D
- // call sys.init 0 
-@RETURN.sys.init.1 	// push return address.
-D=A
-@SP // push d
-M=M+1
-A=M-1
-M=D
-@LCL		// push local
-D=M
-@SP // push d
-M=M+1
-A=M-1
-M=D
-@ARG		// push arg
-D=M
-@SP // push d
-M=M+1
-A=M-1
-M=D
-@THIS		// push this
-D=M
-@SP // push d
-M=M+1
-A=M-1
-M=D
-@5	 		// set D = (SP - (nArgs + 5))
-D=A
-@SP 	
-D=M-D
-@ARG 		// set ARG = D
-M=D
-@SP 		// set LCL = SP
-D=M
-@LCL
-M=D
-@FUNCTION.sys.init 	// goto f
-0; JMP
-(RETURN.sys.init.1)
-
-// ----------------------------------
-
-// ~~~~ SimpleFunction.vm ~~~~
 (FUNCTION.simplefunction.test)
 @SP
 M=M+1
@@ -83,7 +34,6 @@ M=D
 @SP // pop d
 AM=M-1
 D=M
-M=0
 A=A-1
 M=M+D
 
@@ -107,7 +57,6 @@ M=D
 @SP // pop d
 AM=M-1
 D=M
-M=0
 A=A-1
 M=M+D
 
@@ -126,7 +75,6 @@ M=D
 @SP // pop d
 AM=M-1
 D=M
-M=0
 A=A-1
 M=M-D
 
@@ -144,7 +92,6 @@ M=D
 @SP // pop d
 AM=M-1
 D=M
-M=0
 @ARG 	// places return value in the right spot.
 A=M		// goto *(ARG)
 M=D 	// set *ARG = pop()
